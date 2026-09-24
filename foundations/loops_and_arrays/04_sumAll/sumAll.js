2202f734f2a62adcc135b28fdc1ebd00787f476c
Håkon Reinholdtsen <hakonrein@hotmail.com>
2026-09-24 10:39:51 +0200

    Solve exercises on javascript loops and arrays

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR"
+    }
+    let sum = 0
+    for (let i = (a < b ? a : b); i <= (b >= a ? b : a); i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  return Number(((tempFahr - 32) * 5 / 9).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  return Number((tempCels * 9 / 5 + 32).toFixed(1))
 };
 
 // Do not edit below this line
